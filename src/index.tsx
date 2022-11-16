8fdbc2a119f778516576daf5b0530c93ace6e8d8
huyilin <17803000829@163.com>
2022-11-16 09:39:08 +0800

    111

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import "lib-flexible";
 import reportWebVitals from "./reportWebVitals";
 import { isIE, changeBlogTitle } from "@/utils";
 import "@arco-design/web-react/dist/css/arco.css";
+import { message } from "antd";
 
 if (isIE()) {
   alert("请放弃万恶的IE浏览器吧");
@@ -21,6 +22,15 @@ window.onfocus = () => {
 // window.onblur = () => {
 //   document.title = "心 念 · 客官~请留步";
 // };
+
+document.addEventListener("copy", () => {
+  var selecter = window.getSelection()?.toString();
+  if (selecter) {
+    return message.success("阁下，复制成功了！转载要记得标明出处哦～～");
+  }
+
+  message.error("复制失败了，请勾选内容哦");
+});
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
