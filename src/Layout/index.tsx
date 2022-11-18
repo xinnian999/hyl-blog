909afa7fbf9226888e55c79b299cdc408004ba37
hyl <17803000829@163.com>
2022-11-19 03:24:52 +0800

    增加白屏时间显示loading

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -107,6 +107,12 @@ function Layout() {
         },
       });
     }
+
+    const loadingDom: any = document.querySelector("#loading-box");
+    if (loadingDom) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      loadingDom.style.display = "none";
+    }
   }, []);
 
   const renderMenus = useMemo(
