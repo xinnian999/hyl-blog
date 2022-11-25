3ab2917cb6aad0ad7ca61ac7828728da5bd79ed8
hyl <17803000829@163.com>
2022-11-26 02:13:22 +0800

    1

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -94,15 +94,15 @@ function Header() {
       {width > 800 && <ul id="nav">{renderMenus}</ul>}
 
       <div className="header-action">
-        <div className="toolbar">
-          <Tooltip title="切换主题" placement="left">
-            <Dropdown droplist={themeList} trigger="click">
-              <div className="toolbar-item">
-                <BgColorsOutlined />
-              </div>
-            </Dropdown>
-          </Tooltip>
-          {width > 1000 && (
+        {width > 1000 && (
+          <div className="toolbar">
+            <Tooltip title="切换主题" placement="left">
+              <Dropdown droplist={themeList} trigger="click">
+                <div className="toolbar-item">
+                  <BgColorsOutlined />
+                </div>
+              </Dropdown>
+            </Tooltip>
             <Tooltip title="访问网站后台">
               <div
                 className="toolbar-item"
@@ -111,8 +111,8 @@ function Header() {
                 <SettingOutlined />
               </div>
             </Tooltip>
-          )}
-        </div>
+          </div>
+        )}
 
         <div className="user">{loginState ? <User /> : <Login />}</div>
       </div>
