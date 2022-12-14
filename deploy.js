032ac49d9d0bc7e30b05502f86462f0090fbe4b2
hyl <17803000829@163.com>
2022-12-14 16:47:02 +0800

    1

--- deploy.js
+++ deploy.js
@@ -1,20 +1,33 @@
-const scpClient = require('scp2')
-const ora = require('ora')
-const server = {
-    host:'101.42.108.39',//服务器IP
-    port:22,//服务器端口
-    username:'root',//服务器ssh登录用户名
-    password:'Qs991015',//服务器ssh登录密码
-    path:'/www/wwwroot/my-blog'//服务器web目录
-}
-const loading = ora('正在部署至 ' + server.host )
-loading.start()
-scpClient.scp('build/', server ,(err)=>{
-loading.stop()
-    if(err) {
-        console.log('部署失败')
-        throw err
-    }else {
-        console.log('部署成功')
-    }
-})
+const scpClient = require("scp2");
+const ora = require("ora");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const deploy = () =>
+  readline.question(`请输入服务器密码：`, (name) => {
+    const server = {
+      host: "101.42.108.39", //服务器IP
+      port: 22, //服务器端口
+      username: "root", //服务器ssh登录用户名
+      password: name, //服务器ssh登录密码
+      path: "/www/wwwroot/my-blog", //服务器web目录
+    };
+
+    const loading = ora("正在部署至 " + server.host);
+    loading.start();
+    scpClient.scp("build/", server, (err) => {
+      loading.stop();
+
+      if (err) {
+        console.log("密码错误,请重新输入");
+        deploy();
+      } else {
+        console.log("部署成功");
+        readline.close();
+      }
+    });
+  });
+
+deploy();
