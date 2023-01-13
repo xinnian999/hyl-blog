15512b7217c8808b4e91d7a22e7891c29cba1071
hyl <3307578337@qq.com>
2023-01-13 16:38:42 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,10 @@ import { store, persistor } from "@/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { stopWriteLoading } from "@/utils";
 import { useMount } from "@/hooks";
-import { getRandom } from "hyl-utils";
 
 function App() {
   useMount(() => {
     stopWriteLoading();
-
-    console.log(getRandom(1, 20));
-    console.log(getRandom(1, 20));
-    console.log(getRandom(1, 20));
-    console.log(getRandom(1, 20));
-    console.log(getRandom(1, 20));
   });
 
   return (
