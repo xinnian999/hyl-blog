d1d6e6d6d9e5d6a4ea9e6c45494ae1aa83343bee
hyl <3307578337@qq.com>
2023-01-30 16:30:59 +0800

    ok

--- src/views/Demo/index.tsx
+++ src/views/Demo/index.tsx
@@ -1,11 +1,9 @@
 import { Button } from "antd";
-import { Url, getRandom } from "hyl-utils";
+import { Url, Cookie } from "hyl-utils";
 
 export default function Demo() {
   const go = () => {
-    Url.setParams({ name: "hsssyl" + getRandom(1, 500), like: "zaj" }, false);
-    const urlParams = Url.getParams();
-    console.log(urlParams);
+    Cookie.remove("blog_token");
   };
   return <Button onClick={go}>demo</Button>;
 }
