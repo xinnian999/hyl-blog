8e7b76f38701af8f6187d138c3d78cb6e3938fdf
hyl <17803000829@163.com>
2023-03-14 23:20:17 +0800

    ok

--- src/utils/adaptation.ts
+++ src/utils/adaptation.ts
@@ -1,24 +1,31 @@
 const ratio = 54; // 根据项目配置比例的方式自行设定
-const ignore = ["screen", "text-shadow", "box-shadow"]; // 需要排除转换的样式
+const ignore = ["screen", "text-shadow", "box-shadow", "border"]; // 需要排除转换的样式
 
-const ignoreReg = ignore.reduce((str, item) => {
-  return str + "|" + item;
-});
-const adaptation = (component: any) => {
-  const template: string = component.componentStyle.rules[0];
-
-  const newTemplate = template.replace(/.*\d+px\)?/gm, (match) => {
-    if (new RegExp(ignoreReg).test(match)) {
+const r = (str: string) => {
+  return str.replace(/.*\d+px\)?/gm, (match) => {
+    if (ignore.includes(match.trimStart().split(":")[0])) {
       return match;
     }
-    // 其他所有px转化为rem
+
     return match.replace(
       /\d+px/g,
       (m) => (parseInt(m) / ratio).toFixed(2) + "rem"
     );
   });
+};
 
-  component.componentStyle.rules = [newTemplate];
+const adaptation = (component: any) => {
+  const style = component.componentStyle.rules.map((item) => {
+    if (typeof item === "function") {
+      const fnBody = r(item.toString().match(/\{([\s\S]*)\}$/)[1]);
+
+      return new Function("props", fnBody);
+    }
+
+    return r(item);
+  });
+
+  component.componentStyle.rules = style;
 };
 
 export default adaptation;
